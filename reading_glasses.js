1f3af37e01d35a5ebad4e95dc2a68ea89a25e6a5
husky <77258311+floppydiskette@users.noreply.github.com>
2023-03-12 00:59:55 -0800

    Create reading_glasses.js

---
+++ reading_glasses.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name        reading_glasses
+// @namespace   https://github.com/floppydiskette
+// @match       *://*/*
+// @grant       none
+// @version     1.0
+// @author      floppydiskette
+// @description 3/12/2023, 12:26:40 AM
+// ==/UserScript==
+
+function boldenLetters(tag) {
+  const elements = document.getElementsByTagName(tag);
+
+  for (let i = 0; i < elements.length; i++) {
+    let words = elements[i].innerHTML.split(" ");
+    let tag_mode = 0;
+
+      for (let j = 0; j < words.length; j++) {
+        if (words[j].startsWith("<") && tag_mode === 0) {
+          tag_mode = 1;
+        }
+        if (words[j].startsWith("</") && tag_mode === 1) {
+          tag_mode = 2;
+        }
+        if ((words[j].endsWith(">") && tag_mode === 2) || (words[j].includes("</") && words[j].includes(">"))) {
+          tag_mode = 0;
+          continue;
+        }
+
+        if (tag_mode === 0) {
+          let bolden = words[j].length / 2;
+            words[j] = "<b>" + words[j].substring(0, bolden) + "</b>" + words[j].substring(bolden);
+        }
+      }
+      elements[i].innerHTML = words.join(" ");
+
+  }
+}
+
+function idLike() {
+  boldenLetters("p");
+  // boldenLetters("div"); // untested but should work!
+}
+
+window.onload = idLike;
